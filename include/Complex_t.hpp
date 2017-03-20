bcb36961519242f05cd07b13fd7a782f767b0552
Ivanopulopulo <vkuvshinov.ru@gmail.com>
2017-03-20 21:00:23 +0300

    Update Complex_t.hpp

--- include/Complex_t.hpp
+++ include/Complex_t.hpp
@@ -1,20 +1,30 @@
-#include <iostream> 
-#include <stdlib.h> 
-#include <conio.h> 
+#include <iostream>
+
 using namespace std;
 
-class Complex_t{
-	 double a, b;
+class Complex_t
+{
+private:
+	float a;
+	float b;
 public:
-	 Complex_t Read();
-	 Complex_t add(const Complex c1) const ;
-	 Complex_t sub(const Complex c2) const ;
-	 Complex_t multInt(const int n) const ;
-	Complex_t divInt(const int n) const ;
-	Complex_t(double x = 0, double y = 0)
-	{
-		a = x;
-		b = y;
-	}
-	
+	complex_t();
+
+	Complex_t(double x, double y);
+
+	Complex_t(const Complex_t&cop);
+
+	double a_();
+	double b_();
+
+	Complex_t operator * (const Complex_t& c2) const;
+	Complex_t operator / (const Complex_t& c2) const;
+	Complex_t operator += (const Complex_t& c2);
+	Complex_t operator -= (const Complex_t& c2);
+	Complex_t operator *= (const Complex_t& c2);
+	Complex_t operator /= (const Complex_t& c2);
+	Complex_t operator = (const Complex_t& result); 
+	bool operator == (const complex_t& c2) const;
+	Complex_t operator >> ();
+	Complex_t operator << (std::ostream & stream) const;
 };
