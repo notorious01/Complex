45085550a1d4da1a771511a3c58172f918937c8f
Ivanopulopulo <vkuvshinov.ru@gmail.com>
2017-03-20 22:41:16 +0300

    Update сomplex_t.hpp

--- include/сomplex_t.hpp
+++ include/сomplex_t.hpp
@@ -8,15 +8,15 @@ private:
 	double a;
 	double b;
 public:
-	сomplex_t();
+	complex_t();
 	
 	complex_t(double x, double y);
-
+	
 	complex_t(const complex_t&cop);
-
+	
 	double a_();
 	double b_();
-
+	
 	complex_t operator * (const complex_t& c2) const;
 	complex_t operator / (const complex_t& c2) const;
 	complex_t operator += (const complex_t& c2);
