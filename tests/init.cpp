9d1eb15bb92d08aedf42f417472c34178e020884
Ivanopulopulo <vkuvshinov.ru@gmail.com>
2017-03-20 21:00:02 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,60 +1,86 @@
-#include <matrix.hpp>
+#include <Complex_t.hpp>
 #include <catch.hpp>
 
 SCENARIO("def constructor")
 {
-	Matrix matrix;
-	REQUIRE(matrix.Strings_() == 0);
-	REQUIRE(matrix.Columns_() == 0);
+	Complex_t complex;
+	REQUIRE(complex.a_() == 0);
+	REQUIRE(complex.b_() == 0);
 }
 
 SCENARIO("param constructor") 
 {
-	Matrix matrix(3, 3);
-	REQUIRE(matrix.Strings_() == 3);
-	REQUIRE(matrix.Columns_() == 3);
+	Complex_t complex(1, 2);
+	REQUIRE(complex.a_() == 1);
+	REQUIRE(complex.b_() == 2);
 }
 
-SCENARIO("copy constructor") 
+SCENARIO("copy constructor")
 {
-	Matrix matrix (3, 3);	
-	Matrix copy(matrix);	
-	REQUIRE(copy.Strings_() == 3);
-	REQUIRE(copy.Columns_() == 3);
+	Complex_t complex(1, 2);
+	Complex_t copy(complex);
+	REQUIRE(copy.a_() == 1);
+	REQUIRE(copy.b_() == 2);
 }
 
-
-SCENARIO("operator +") 
+SCENARIO("operator *")
 {
-	Matrix matrix1 (3, 3);
-	Matrix matrix2 (3, 3);
-	Matrix matrix3 (3, 3);	
-	std::ifstream("matrix1.txt") >> matrix1;
-	std::ifstream("matrix2.txt") >> matrix2;
-	std::ifstream("summatrix.txt") >> matrix3;
-	REQUIRE(matrix1 + matrix2 == matrix3);
+	Complex_t c1(3, 2);
+	Complex_t c2(5, 4);
+	Complex_t c3(15, 8);
+	REQUIRE((c1*c2) == c3);
 }
 
-SCENARIO("operator *") 
+SCENARIO("operator /")
 {
-	Matrix matrix1 (3, 3);
-	Matrix matrix2 (3, 3);
-	Matrix matrix3 (3, 3);	
-	std::ifstream("matrix1.txt") >> matrix1;
-	std::ifstream("matrix2.txt") >> matrix2;
-	std::ifstream("multmatrix.txt") >> matrix3;
-	REQUIRE(matrix1 * matrix2 == matrix3);
-}
-SCENARIO("operator =") 
-{
-	Matrix matrix1(3, 3);
-	Matrix matrix2 = matrix1;
-	REQUIRE(matrix1 == matrix2);
-}
-SCENARIO("operator ==")
-{
-	Matrix matrix1(3, 3);
-	Matrix matrix2(3, 3);
-	REQUIRE(matrix1 == matrix2);
+	Complex_t c1(4, 5);
+	Complex_t c2(4, 5);
+	Complex_t c3(1, 0);
+	REQUIRE((c1 / c2) == c3);
 }
 
+SCENARIO("operator +=")
+{
+	Complex_t c1(11, 1);
+	Complex_t c2(8, 3);
+	Complex_t c3(19, 4);
+	REQUIRE((c1 += c2) == c3);
+}
+
+SCENARIO("operator -=")
+{
+	Complex_t c1(6, 10);
+	Complex_t c2(3, 6);
+	Complex_t c3(3, 4);
+	REQUIRE((c1 -= c2) == c3);
+}
+
+SCENARIO("operator *=")
+{
+	Complex_t c1(7, 6);
+	Complex_t c2(5, 4);
+	Complex_t c3(11, 58);
+	REQUIRE((c1 *= c2) == c3);
+}
+
+SCENARIO("operator /=")
+{
+	Complex_t c1(2, 1);
+	Complex_t c2(2, 1);
+	Complex_t c3(1, 0);
+	REQUIRE((c1 /= c2) == c3);
+}
+
+SCENARIO("operator =")
+{
+	Complex_t c1(5, 7);
+	Complex_t c2 = c1;
+	REQUIRE(c2 == c1);
+}
+
+SCENARIO("operator ==") 
+{
+	Complex_t c1(2, 5);
+	Complex_t c2(2, 5);
+	REQUIRE(c1 == c2);
+}
