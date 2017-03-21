be3b60264ef9f77263fb27893113d59d167ce8cd
Ivanopulopulo <vkuvshinov.ru@gmail.com>
2017-03-21 14:25:59 +0400

    Update сomplex_t.hpp

--- include/сomplex_t.hpp
+++ include/сomplex_t.hpp
@@ -14,16 +14,16 @@ public:
 	
 	complex_t(const complex_t&cop);
 	
-	const double a_();
-	const double b_();
+	double a_();
+	double b_();
 	
 	complex_t operator * (const complex_t& c2) const;
 	complex_t operator / (const complex_t& c2) const;
-	complex_t operator += (const complex_t& c2); const
-	complex_t operator -= (const complex_t& c2) const;
-	complex_t operator *= (const complex_t& c2) const;
-	complex_t operator /= (const complex_t& c2) const;
-	complex_t operator = (const complex_t& result) const;
+	complex_t operator += (const complex_t& c2); 
+	complex_t operator -= (const complex_t& c2) ;
+	complex_t operator *= (const complex_t& c2) ;
+	complex_t operator /= (const complex_t& c2) ;
+	complex_t operator = (const complex_t& result);
 	bool operator == (const complex_t& c2) const;
 	friend istream& operator >> (istream&cin, complex_t& result);
 	friend ostream& operator << (ostream&cout, complex_t& result);
