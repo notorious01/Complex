9c998b5f6151f2c4ae2aabb636d655a40ac3a250
notorious01 <tuborg1604@gmail.com>
2017-03-27 02:32:14 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,85 +1,84 @@
-#include <сomplex_t.hpp>
+#include <Complex_t.hpp>
 #include <catch.hpp>
 SCENARIO("def constructor")
 {
-	complex_t Complex;
-	REQUIRE(Complex.a_() == 0);
-	REQUIRE(Complex.b_() == 0);
+	Complex_t Complex;
+	REQUIRE(Complex.real_() == 0);
+	REQUIRE(Complex.image_() == 0);
 }
 
 SCENARIO("param constructor")
 {
-	complex_t Complex(1, 2);
-	REQUIRE(Complex.a_() == 1);
-	REQUIRE(Complex.b_() == 2);
-}
+	Complex_t Complex(1, 2);
+	REQUIRE(Complex.real_() == 1);
+	REQUIRE(Complex.image_() == 2);
 
 SCENARIO("copy constructor")
 {
-	complex_t Complex(1, 2);
-	complex_t cop(Complex);
-	REQUIRE(cop.a_() == 1);
-	REQUIRE(cop.b_() == 2);
+	Complex_t Complex(1, 2);
+	Complex_t copy(Complex);
+	REQUIRE(copy.real_() == 1);
+	REQUIRE(copy.image_() == 2);
 }
 
 SCENARIO("operator *")
 {
-	complex_t c1(7, 6);
-	complex_t c2(5, 4);
-	complex_t c3(11, 58);
-	REQUIRE((c1*c2) == c3);
+	Complex_t m1(7, 6);
+	Complex_t m2(5, 4);
+	Complex_t m3(11, 58);
+	REQUIRE((m1*m2) == m3);
 }
 
 SCENARIO("operator /")
 {
-	complex_t c1(4, 5);
-	complex_t c2(4, 5);
-	complex_t c3(1, 0);
-	REQUIRE((c1 / c2) == c3);
+	Complex_t m1(4, 5);
+	Complex_t m2(4, 5);
+	Complex_t m3(1, 0);
+	REQUIRE((m1 / m2) == m3);
 }
 
 SCENARIO("operator +=")
-{
-	complex_t c1(11, 1);
-	complex_t c2(8, 3);
-	complex_t c3(19, 4);
-	REQUIRE((c1 += c2) == c3);
+{ 
+	Complex_t m1(11, 1);
+	Complex_t m2(8, 3);
+	Complex_t m3(19, 4);
+	REQUIRE((m1 +=m2) == m3); 
 }
 
 SCENARIO("operator -=")
-{
-	complex_t c1(6, 10);
-	complex_t c2(3, 6);
-	complex_t c3(3, 4);
-	REQUIRE((c1 -= c2) == c3);
+{ 
+	Complex_t m1(6, 10);
+	Complex_t m2(3, 6);
+	Complex_t m3(3, 4);
+	REQUIRE((m1 -=m2) == m3); 
 }
 
 SCENARIO("operator *=")
 {
-	complex_t c1(7, 6);
-	complex_t c2(5, 4);
-	complex_t c3(11, 58);
-	REQUIRE((c1 *= c2) == c3);
+	Complex_t m1(7, 6);
+	Complex_t m2(5, 4);
+	Complex_t m3(11, 58);
+	REQUIRE((m1 *= m2) == m3);
 }
 
 SCENARIO("operator /=")
 {
-	complex_t c1(2, 1);
-	complex_t c2(2, 1);
-	complex_t c3(1, 0);
-	REQUIRE((c1 /= c2) == c3);
+	Complex_t m1(2,1);
+	Complex_t m2(2,1);
+	Complex_t m3(1,0);
+	REQUIRE((m1 /= m2) == m3);
 }
 
 SCENARIO("operator =")
 {
-	complex_t c1(5, 7);
-	complex_t c2 = c1;
-	REQUIRE(c2 == c1);
+	Complex_t m1(5, 7);
+	Complex_t m2 = m1;
+	REQUIRE (m2 == m1);
 }
 
 SCENARIO("operator ==")
 {
-	complex_t c1(2, 5);
-	complex_t c2(2, 5);
-	REQUIRE(c1 == c2);
+	Complex_t m1(2, 5);
+	Complex_t m2(2, 5);
+	REQUIRE(m1 == m2);
 }
