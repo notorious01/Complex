e758436bfafadc233f9912e7730c847e93ac5ac4
notorious01 <tuborg1604@gmail.com>
2017-03-27 11:32:39 +0300

    Update сomplex_t.hpp

--- include/сomplex_t.hpp
+++ include/сomplex_t.hpp
@@ -22,6 +22,6 @@ public:
 	complex_t operator /= (const complex_t& m2);
 	complex_t operator = (const complex_t& result);
 	bool operator == (const complex_t& m2) const;
-	friend istream& operator >> (istream&cin,complex_t& result);
-	friend ostream& operator << (ostream&cout,const complex_t& result);
+	friend istream& operator >> (istream&,complex_t& result);
+	friend ostream& operator << (ostream&,const complex_t& result) const;
 };
