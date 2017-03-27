e8880dd2e12f515e5a9d8fb2ad6ad941dbc6b1ce
notorious01 <tuborg1604@gmail.com>
2017-03-27 03:27:16 +0300

    Update сomplex_t.hpp

--- include/сomplex_t.hpp
+++ include/сomplex_t.hpp
@@ -2,24 +2,24 @@
 
 using namespace std;
 
-class Complex_t
+class complex_t
 {
 private:
 	double real, image;
 public:
-	Complex_t();
-	Complex_t(double r, double im);
-	Complex_t(const complex_t&copy);
+	complex_t();
+	complex_t(double r, double im);
+	complex_t(const complex_t&copy);
 	double real_() const;
 	double image_() const;
 	
-	Complex_t operator * (const Complex_t& m2) const;
-	Complex_t operator / (const Complex_t& m2) const;
-	Complex_t operator += (const Complex_t& m2);
-	Complex_t operator -= (const Complex_t& m2);
-	Complex_t operator *= (const Complex_t& m2);
-	Complex_t operator /= (const Complex_t& m2);
-	Complex_t operator = (const Complex_t& result);
+	complex_t operator * (const complex_t& m2) const;
+	complex_t operator / (const complex_t& m2) const;
+	complex_t operator += (const complex_t& m2);
+	complex_t operator -= (const complex_t& m2);
+	complex_t operator *= (const complex_t& m2);
+	complex_t operator /= (const complex_t& m2);
+	complex_t operator = (const complex_t& result);
 	bool operator == (const Complex_t& m2) const;
 	friend istream& operator >> (istream&cin, complex_t& result;
 	friend ostream& operator << (ostream&cout, const complex_t& result);
